4b37da35161e8c41c169af053b3cabbe9b5de5fd
JoshMusira <joshuamusira01@gmail.com>
2023-05-19 21:29:27 +0300

    Fixing css

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -1,38 +1,55 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
+const FetchData = () => {
+  const [records, setRecords] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://universities.hipolabs.com/search?country=${searchQuery}`
+        );
+        const data = await response.json();
+        setRecords(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
+    fetchData();
+  }, [searchQuery]);
 
-const FetchData = () =>{
-    const [records, setRecords] = useState([])
+  const handleSearch = () => {
+    const input = document.getElementById("searchInput");
+    setSearchQuery(input.value);
+  };
 
-    useEffect(() =>{
-        fetch("http://universities.hipolabs.com/search?country")
-        .then(res => {
-            return res.json();
-        })
-        .then(data  => setRecords(data))
-        .catch(err => console.log(err))
-    },[])
+  return (
+    <main>
+      <div className="header">
+        <span>Current Universities</span>
+        <div className="subHeader">
+          <input
+            id="searchInput"
+            type="text"
+            placeholder="Type any country..."
+          />
+          <button onClick={handleSearch}>Search...</button>
+        </div>
+      </div>
 
-    return (
-        <main>
-            <div className="header">
-                    <span>Current Universities</span>
-                    <div className="subHeader">
-                        <input type="text" placeholder="Type any country..." />
-                        <button>Search...</button>
-                    </div>
-            </div>
-            
-                <div className="container">
-                    {records.map((item,index) =>(
-                        <div key = {index} className="contain"><p >Name:{item.name} </p> <p>web_page:{item.web_pages}</p> <p>country: {item.country}</p></div>
-                    ))}
-            </div>
-
-        </main>
-    )    
+      <div className="container">
+        {records.map((item, index) => (
+          <div key={index} className="contain">
+            <p>Name: {item.name}</p>
+            <p>Web Page: {item.web_pages}</p>
+            <p>Country: {item.country}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default FetchData;
